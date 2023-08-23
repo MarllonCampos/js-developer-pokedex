466ccf40dc7f93efc07dbdc395f7762914a3e014
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-08-23 02:49:06 -0300

    feat: add some attributes for pokemon model

--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -1,8 +1,19 @@
-
 class Pokemon {
-    number;
-    name;
-    type;
-    types = [];
-    photo;
+  number;
+  name;
+  type;
+  types = [];
+  photo;
+  abilities = [];
+  stats = {
+    height: 0,
+    weight: 0,
+    hp: 0,
+    attack: 0,
+    defense: 0,
+    spAtk: 0,
+    spDef: 0,
+    speed: 0,
+    total: 0,
+  };
 }
